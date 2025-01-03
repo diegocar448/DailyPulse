66f54bc842a598896f45e256b413212a8c47e606
Diego Cardoso <diegocar448@hotmail.com>
2025-01-03 15:36:56 -0300

    initials files

---
+++ shared/src/commonMain/kotlin/com/diego/dailypulse/Greeting.kt
@@ -0,0 +1,9 @@
+package com.diego.dailypulse
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
