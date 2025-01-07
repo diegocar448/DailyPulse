1aa1147c2b7b6b534b1981329c58eecf6aa24ce4
Diego Cardoso <diegocar448@hotmail.com>
2025-01-07 16:51:38 -0300

    ArticleViewModelP2

--- shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesViewModel.kt
@@ -1,12 +1,27 @@
 package com.diego.dailypulse.articles
 
 import com.diego.dailypulse.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ArticlesViewModel: BaseViewModel() {
 
     private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
 
     val articlesState: StateFlow<ArticlesState> get() = _articlesState
+
+    init{
+        getArticles()
+    }
+
+    //fun retornar os artigos
+    private fun getArticles(){
+        //codigo que executara sem bloquear a thread principal
+        scope.launch {
+            delay(500)
+            _articlesState.emit(ArticlesState())
+        }
+    }
 }
