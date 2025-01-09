f547859fedd421fc8b5c7f7b2d50f4148af783b6
Diego Cardoso <diegocar448@hotmail.com>
2025-01-09 14:25:08 -0300

    Article Entity and Article State implementation

---
+++ shared/src/commonMain/kotlin/com/diego/dailypulse/articles/Article.kt
@@ -0,0 +1,8 @@
+package com.diego.dailypulse.articles
+
+data class Article(
+    val title: String,
+    val desc: String,
+    val date: String,
+    val imageUrl: String,
+)
--- shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesState.kt
+++ shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesState.kt
@@ -1,4 +1,8 @@
 package com.diego.dailypulse.articles
 
-class ArticlesState {
-}
+data class ArticlesState(
+    val articles: List<Article> = listOf(),
+    val loading: Boolean = false,
+    val error: String? = null
+)
+
