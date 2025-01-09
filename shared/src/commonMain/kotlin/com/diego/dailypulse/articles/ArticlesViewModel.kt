b5988c9daf7c7f293d0de93cc277188a64e61edc
Diego Cardoso <diegocar448@hotmail.com>
2025-01-09 16:40:23 -0300

    Finishing the View Model implementation

--- shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/diego/dailypulse/articles/ArticlesViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.launch
 
 class ArticlesViewModel: BaseViewModel() {
 
-    private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
+    private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState(loading = true))
 
     val articlesState: StateFlow<ArticlesState> get() = _articlesState
 
@@ -20,8 +20,36 @@ class ArticlesViewModel: BaseViewModel() {
     private fun getArticles(){
         //codigo que executara sem bloquear a thread principal
         scope.launch {
+            val fetchedArticles = fetchArticles()
+
             delay(500)
-            _articlesState.emit(ArticlesState())
+
+            _articlesState.emit(ArticlesState(articles = fetchedArticles))
         }
     }
+
+
+
+    suspend fun fetchArticles(): List<Article> = mockArticles
+
+    private val mockArticles = listOf(
+        Article(
+            "Stock market today: Live updates - CNBC",
+            "Futures were higher in premarket trading as Wall Street tried to regain its footing.",
+            "2023-11-09",
+            imageUrl = "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpg"
+        ),
+        Article(
+            "Best Iphone Deals (2023): Carrier Deals, Unlocked iPhones",
+            "Apple's smartphone rarely go on sale, but if you're looking to upgrade (or you're gift shopping), here.",
+            "2023-11-09",
+            imageUrl = "https://media.craiyon.com/2023-10-21/1f1d408dddce4984a689afc06d9f42d8.webp"
+        ),
+        Article(
+            "What is Lorem Ipsum?",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+            "2023-11-09",
+            imageUrl = "https://imagen.research.google/main_gallery_images/a-robot-couple-fine-dining.jpg"
+        ),
+    )
 }
